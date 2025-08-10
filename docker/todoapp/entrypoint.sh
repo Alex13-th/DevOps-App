76f957d5af3d351ccfa0bf9084111b18ee07c27a
Alex13-th <aleks.shaparenko@gmail.com>
2025-08-10 20:01:16 +0200

    fix bugs

--- docker/todoapp/entrypoint.sh
+++ docker/todoapp/entrypoint.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env sh
 set -eu
 
+export DJANGO_SETTINGS_MODULE="todolist.settings"
+export PYTHONPATH="/app"
+
 echo "⏳ Waiting for DB (via Django)..."
 python - <<'PY'
-import time, sys, django
+import os, time, sys, django
 from django.db import connections
 from django.db.utils import OperationalError
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todolist.settings")
+
 django.setup()
-for _ in range(60):
+
+deadline = time.time() + 60
+while time.time() < deadline:
     try:
-        connections['default'].cursor()
+        connections["default"].cursor()
         sys.exit(0)
     except OperationalError:
         time.sleep(1)
+
+print("DB is not ready after 60s", file=sys.stderr)
 sys.exit(1)
 PY
 
@@ -21,3 +31,5 @@ python manage.py migrate --noinput
 
 echo "🚀 Starting server..."
 exec python manage.py runserver 0.0.0.0:8080
+
+
